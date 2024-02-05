e5c37c2d5f4e19f732bf4db6bbe5c666e0fc1e00
Scythe7 <joachimgcontreras@gmail.com>
2024-02-05 13:21:05 +1300

    Update API

--- script.js
+++ script.js
@@ -1,47 +1,55 @@
 function updateCountdown() {
-    fetch('https://worldtimeapi.org/api/timezone/Pacific/Auckland')
-      .then(response => response.json())
-      .then(data => {
-        var now = new Date(data.datetime);
+    var now = new Date();
+    
+    // Get the timezone offset
+    var timezoneOffset = now.getTimezoneOffset();
+    
+    // Convert the offset to hours and minutes
+    var hours = Math.abs(Math.floor(timezoneOffset / 60));
+    var minutes = Math.abs(timezoneOffset % 60);
+    
+    // Determine the sign of the offset
+    var sign = timezoneOffset > 0 ? "-" : "+";
   
-        var periods = [
-          { name: "Period 1", time: "08:45" },
-          { name: "Period 2", time: "09:40" },
-          { name: "Break", time: "10:40" },
-          { name: "Period 3", time: "11:00" },
-          { name: "Homeroom", time: "12:00" },
-          { name: "Period 4", time: "12:20" },
-          { name: "Lunch", time: "13:20" },
-          { name: "Period 5", time: "14:20" },
-          { name: "School ends", time: "15:20" }
-        ];
+    // Construct the timezone name
+    var timezoneName = "GMT" + sign + pad(hours) + ":" + pad(minutes);
+    
+    console.log("Timezone:", timezoneName);
   
-        var closestPeriod = null;
-        for (var i = 0; i < periods.length; i++) {
-          var periodTime = new Date(now.toDateString() + " " + periods[i].time);
-          if (periodTime.getTime() > now.getTime()) {
-            closestPeriod = periods[i];
-            break;
-          }
-        }
+    var periods = [
+      { name: "Period 1", time: "08:45" },
+      { name: "Period 2", time: "09:40" },
+      { name: "Break", time: "10:40" },
+      { name: "Period 3", time: "11:00" },
+      { name: "Homeroom", time: "12:00" },
+      { name: "Period 4", time: "12:20" },
+      { name: "Lunch", time: "13:20" },
+      { name: "Period 5", time: "14:20" },
+      { name: "School ends", time: "15:20" }
+    ];
   
-        if (closestPeriod) {
-          var timeDiff = periodTime.getTime() - now.getTime();
-          var hours = Math.floor(timeDiff / (1000 * 60 * 60));
-          var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-          var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+    var closestPeriod = null;
+    for (var i = 0; i < periods.length; i++) {
+      var periodTime = new Date(now.toDateString() + " " + periods[i].time);
+      if (periodTime.getTime() > now.getTime()) {
+        closestPeriod = periods[i];
+        break;
+      }
+    }
   
-          var countdownStr = closestPeriod.name + " starts in ";
-          countdownStr += hours + "h " + minutes + "m " + seconds + "s";
+    if (closestPeriod) {
+      var timeDiff = periodTime.getTime() - now.getTime();
+      var hours = Math.floor(timeDiff / (1000 * 60 * 60));
+      var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
   
-          document.getElementById("countdown").innerText = countdownStr;
-        } else {
-          document.getElementById("countdown").innerText = "School has finished for today.";
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching time data:', error);
-      });
+      var countdownStr = closestPeriod.name + " starts in ";
+      countdownStr += hours + "h " + minutes + "m " + seconds + "s";
+  
+      document.getElementById("countdown").innerText = countdownStr;
+    } else {
+      document.getElementById("countdown").innerText = "School has finished for today.";
+    }
   }
   
   // Update countdown every second
@@ -49,4 +57,12 @@ function updateCountdown() {
   
   // Initial update
   updateCountdown();
+  
+  // Helper function to pad single-digit numbers with leading zeros
+  function pad(number) {
+    if (number < 10) {
+      return "0" + number;
+    }
+    return number;
+  }
   
